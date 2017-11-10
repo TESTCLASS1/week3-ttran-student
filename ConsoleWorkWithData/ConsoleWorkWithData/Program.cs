317c82dab57a82bf95f9f4bfc968d971383f89c4
Tuan Tran <tuantran.osu@gmail.com>
2017-11-10 11:39:13 -0500

    Update Program.cs

--- ConsoleWorkWithData/ConsoleWorkWithData/Program.cs
+++ ConsoleWorkWithData/ConsoleWorkWithData/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
 
 namespace ConsoleWorkWithData
@@ -11,23 +7,43 @@ namespace ConsoleWorkWithData
     {
         static void Main(string[] args)
         {
-            string FirstName = "Tuan"; //string data type - i don't know if this is correct - no, add value to the string
-            string LastName = "Tran"; //string data type - same here - add value to the string variable here too
-            int StudentID = 8079; //int data type
+            //declare variables:
+            //========================================================================================================
+            string FirstName = "Tuan"; //string data type - you declare variable "FirstName" and assign a value "Tuan"
+            // Your Task: do the same as above: declare avariable "LastName" of string data type and assign a value 
+            // (your last name - remember to have it within double quote) 
+            
+            int StudentID = 12345; //you declare a variable of StudentID of int data type and assign a value of "12345"
+            //note: there is no quote for numbers
+            
             DateTime BirthDate = new DateTime(1991, 03, 04); //date time data type 
+            
             decimal Grade = 95m; //decimal data type 
+            
+            //display information on the console:
+            //========================================================================================================
+            //use WriteLine() command to display information
+            // the following command will display "a. --- --- ------------------ ---------- ------------- -------- ----"
+            //on console and move cursor to the next line
             WriteLine("a. --- --- ------------------ ---------- ------------- -------- ----");
-            WriteLine("b.  My name is "+LastName+' '+FirstName); //string data type 
-            WriteLine("c.     I am a new student and this is my first program.");
-            WriteLine("d. *** *** ****************** ********** ************* ******** ****");
-            WriteLine("e.  My StudentID number is " + StudentID); //int data type
-            WriteLine("f.     My birthdate is " +BirthDate.ToString("MM/dd/yyyy")); //date time data type 
-            WriteLine("g.        My current grade in the course is a " + Grade); //decimal data type
-            WriteLine("h. ___ ___ _________________ _________ _____________ _________ ____");
-            Read();
+            
+            //If you want to display fixed text with variables, we can use "+" as below
+            //display fixed string "b.  My name is " with values of the variables of FirstName and LastName
+            WriteLine("b.  My name is " + FirstName + ' ' + LastName);
+            
+            //Your Task: using WriteLine() to display information of
+            1. "c. I am a new student and this is my first program."
+            2. "d. *** *** ****************** ********** ************* ******** ****"
+            3. "e. My StudentID number is " with information stored in variable StudentID (hint: you should use "+")
+            4. "f. My birthdate is " with information stored in variable BirthDate of DateTime data type
+            //here we use a convertion function to convert a DateTime value to string: BirthDate.ToString("MM/dd/yyyy")); 
+            // use "+" to combine fixed string with converted value 
+            
+            5. "g.        My current grade in the course is a " with value of grade stored in Grade variable
+            6. "h. ___ ___ _________________ _________ _____________ _________ ____"
+            
+            7. Finally, we use Read() command to freeze the console so we can see the output of WriteLine() commands above
 
-            //everything looks good! go ahead to submit it! thanks.
-            //thank you - my final code!
         }
     }
 }
